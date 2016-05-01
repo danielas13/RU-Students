43c512fe61f1c0954d464eff4ca96c78066370de
Daniel.A <danielas13@ru.is>
2016-05-01 19:37:54 +0000

    Meta/cs files included.

---
+++ Argus/Assets/game.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class game : MonoBehaviour {
+    public static game gm;
+
+    void Start()
+    {
+        if(gm == null)
+        {
+            gm = GameObject.FindGameObjectWithTag("game").GetComponent<game>();
+        }
+    }
+    public static void KillPlayer(Stats player)
+    {
+        Destroy(player.gameObject);
+        gm.playerRespawn();
+
+
+    }
+
+    public Transform playerPrefab, spawn;
+    public void playerRespawn()
+    {
+        Instantiate(playerPrefab, spawn.position, spawn.rotation);
+    }
+}
