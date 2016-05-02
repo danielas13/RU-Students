02607c6b455cc926afe1e6198ddd1c66082cf8e9
Daniel.A <danielas13@ru.is>
2016-05-02 21:26:26 +0000

    Ai pathfinding

--- Argus/Assets/EnemyBehavior.cs
+++ Argus/Assets/EnemyBehavior.cs
@@ -1,42 +1,132 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class EnemyBehavior : MonoBehaviour {
+public class EnemyBehavior : MonoBehaviour
+{
 
     public bool movementDirection = true;
-    public float movementVelocity = 2.5f;
+    public float movementVelocity = 1f;
     Transform trackPoint;
+    //enemy patrol/pathfinding variables.
     public LayerMask NotHit;
     public float fallDistance = 1f;
     public float collideDistance = 0.5f;
     public float direction = -1f;
 
+    //Enemy chase variables
+    public float aggroRange = 5f;
+    public LayerMask aggroLayers;
+    GameObject player;
+    private bool chase = false;
+    public float chasingVelocity = 2f;
+
     // Use this for initialization
-    void Start () {
+    void Start()
+    {
+        player = GameObject.FindGameObjectWithTag("Player");
         trackPoint = transform.FindChild("trackPoint");
 
     }
-	
-	// Update is called once per frame
-	void Update() {
-        
-        Vector2 trackPosition = new Vector2(trackPoint.position.x, trackPoint.position.y);
-        RaycastHit2D hitDown = Physics2D.Raycast(trackPosition, new Vector2(0,-1), fallDistance, NotHit);
-        RaycastHit2D hitForwards = Physics2D.Raycast(trackPosition, new Vector2(1, 0), collideDistance, NotHit);
-        //Debug.Log(hit.distance);
-        if (hitDown.collider != null && hitForwards.collider == null)
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        if (player == null)
         {
-            //The ai has hit a wall/floor
-            //Debug.Log(hitDown.distance);
+            player = GameObject.FindGameObjectWithTag("Player");
+        }
+
+        Vector2 playerPos = new Vector2(player.transform.position.x, player.transform.position.y);
+        Vector2 enemyPos = new Vector2(transform.position.x, transform.position.y);
+        Vector2 calculateAngle = playerPos - enemyPos;
+
+        RaycastHit2D rayToPlayer = Physics2D.Raycast(enemyPos, calculateAngle, aggroRange, aggroLayers);
+
+        Vector2 trackPosition = new Vector2(trackPoint.position.x, trackPoint.position.y);
+        RaycastHit2D hitDown = Physics2D.Raycast(trackPosition, new Vector2(0, -1), fallDistance, NotHit);
+        RaycastHit2D hitForwards = Physics2D.Raycast(trackPosition, new Vector2(1, 0), collideDistance, NotHit);
+
+        //Check if the unit should enter chase mode.
+        if (rayToPlayer.collider != null)
+        {
+            if(rayToPlayer.collider.gameObject.layer == 10)
+            {
+                Debug.DrawLine(transform.position, player.transform.position, Color.red);
+            }
+            else if(rayToPlayer.collider.gameObject.layer == 8)
+            {
+                this.chase = true;
+                Debug.DrawLine(transform.position, player.transform.position, Color.blue);
+            }
+            else
+            {
+                this.chase = false;
+            }
         }
         else
         {
-            direction = -direction;
-            //trackPoint.transform.position = new Vector3(-trackPoint.position.x, trackPoint.position.y, trackPoint.position.z);
-            transform.localScale = new Vector3(-transform.localScale.x, 1f, 1f);
-            Debug.Log("posX : " + trackPoint.position.x + "Pos y : " + trackPoint.position.y + "Pos z  : " + trackPoint.position.z);
+            this.chase = false;
         }
-        transform.Translate(Vector3.right * Time.deltaTime * 1f * direction);
 
+        if (!this.chase)
+        {
+            if (hitDown.collider != null && hitForwards.collider == null)
+            {
+
+            }
+            else
+            {
+               direction = -direction;
+               transform.localScale = new Vector3(-transform.localScale.x, 1f, 1f);
+             }
+            transform.Translate(Vector3.right * Time.deltaTime * movementVelocity * direction);
+        }
+        else
+        {
+            //The player is to the right.
+            if((Mathf.Abs(playerPos.x - enemyPos.x)) < 1.0f){
+                if(playerPos.x > enemyPos.x)
+                {
+                    if (direction != 1)
+                    {
+                        direction = -direction;
+                        transform.localScale = new Vector3(-transform.localScale.x, 1f, 1f);
+                    }
+                }
+                else if(playerPos.x < enemyPos.x)
+                {
+                    if (direction == 1)
+                    {
+                        direction = -direction;
+                        transform.localScale = new Vector3(-transform.localScale.x, 1f, 1f);
+                    }
+                }
+            }
+           else  if(playerPos.x > enemyPos.x)
+            {
+                if (direction != 1)
+                {
+                    direction = -direction;
+                    transform.localScale = new Vector3(-transform.localScale.x, 1f, 1f);
+                }
+                if (hitDown.collider != null && hitForwards.collider == null) 
+                {
+                    transform.Translate(Vector3.right * Time.deltaTime * chasingVelocity * direction);
+                }
+            }
+            else if(playerPos.x < enemyPos.x)
+            {
+                if (direction == 1)
+                {
+                    direction = -direction;
+                    transform.localScale = new Vector3(-transform.localScale.x, 1f, 1f);
+                }
+                if (hitDown.collider != null && hitForwards.collider == null)
+                {
+                    transform.Translate(Vector3.right * Time.deltaTime * chasingVelocity * direction);
+                }      
+            }
+        }
     }
 }
