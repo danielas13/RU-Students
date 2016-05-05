c2373b52ac1081c9410ef127fb7873ab66855e66
dorisnaer <dorisnaer@gmail.com>
2016-05-05 18:43:33 +0000

    Tutorial finished

---
+++ Argus/Assets/ChangeSpawnScript.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ChangeSpawnScript : MonoBehaviour {
+
+	public Transform TargetObject;
+
+	//Will trigger if the player is withing the door boundaries and pressesthe up key.
+	void OnTriggerStay2D(Collider2D other)
+	{
+		if (Input.GetKeyDown(KeyCode.UpArrow) && other.gameObject.tag == "Player")
+		{
+
+			GameObject startingDoor = GameObject.Find("StartingDoor");
+			startingDoor.GetComponent<OneToOneDoor>().TargetObject = TargetObject;
+
+			GameObject player = GameObject.Find("Player");
+			player.transform.position = new Vector3(TargetObject.position.x, TargetObject.position.y, player.transform.position.z);
+			GameObject camera = GameObject.Find("MainCamera");
+			camera.transform.position = player.transform.position;
+		}
+	}
+}
--- Argus/Assets/Spawners/SpawnerScripts/EnemySpawn.cs
+++ Argus/Assets/Spawners/SpawnerScripts/EnemySpawn.cs
@@ -15,6 +15,8 @@ public class EnemySpawn : MonoBehaviour {
     private float currentCount = 0;   //The current counter for spawning enemies.
     private List<Object> lis = new List<Object>();  //The list containing our objects.
     GameObject player;                //The Player object.
+	public bool SingleSpawn = false;
+	private bool canSpawn = true;
 
     // Use this for initialization
     void Start () {
@@ -24,50 +26,49 @@ public class EnemySpawn : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-        //fidning the player object in case the player has respawned.
-        if (player == null)
-        {
-            player = GameObject.FindGameObjectWithTag("Player");
-        }
+		if(canSpawn){
+			//fidning the player object in case the player has respawned.
+			if (player == null)
+			{
+				player = GameObject.FindGameObjectWithTag("Player");
+			}
+			//Checking current amount of spawns.
+			if (this.TotalSpawns > this.lis.Count) {
+				//Updating the timer if it's on cooldown.
+				if (this.currentCount > 0) {
+					currentCount -= Time.deltaTime;
+				} else if (this.currentCount <= 0) {
+					//Creating the ray towards player to check if he is in sight.
+					Vector2 playerPos = new Vector2 (player.transform.position.x, player.transform.position.y);
+					Vector2 enemyPos = new Vector2 (transform.position.x, transform.position.y);
+					RaycastHit2D rayToPlayer = Physics2D.Raycast (enemyPos, playerPos - enemyPos, MaxRangeFromPlayer, spawnLayers);
 
-        //Checking current amount of spawns.
-        if (this.TotalSpawns > this.lis.Count)
-        {
-            //Updating the timer if it's on cooldown.
-            if(this.currentCount > 0)
-            {
-                currentCount -= Time.deltaTime;
-            }
-            else if(this.currentCount <= 0)
-            {
-                //Creating the ray towards player to check if he is in sight.
-                Vector2 playerPos = new Vector2(player.transform.position.x, player.transform.position.y);
-                Vector2 enemyPos = new Vector2(transform.position.x, transform.position.y);
-                RaycastHit2D rayToPlayer = Physics2D.Raycast(enemyPos, playerPos - enemyPos, MaxRangeFromPlayer, spawnLayers);
-
-                //Check if the player is in range.
-                if(rayToPlayer.collider == null)
-                {
-                    //Reset the timer.
-                    this.currentCount = this.coolDown;
-                    //instantiate the new object ( create a new clone of the enemy prefab).
-                    Object newObj = Instantiate(EnemyPrefab, transform.position, transform.rotation);
-                    //add the new enemy object to the list.
-                    this.lis.Add(newObj);
-                }
-            }
-        }
-        //There are already maximum amounts of spawns.
-        else
-        {
-            //iterate through the list of objects and remove objects that have been destroyed from the list.
-            for(int i = 0; i < this.lis.Count;i++)
-            {
-                if(this.lis[i] == null)
-                {
-                    this.lis.Remove(this.lis[i]);
-                }
-            }
-        }
+					//Check if the player is in range.
+					if (rayToPlayer.collider == null) {
+						//Reset the timer.
+						this.currentCount = this.coolDown;
+						//instantiate the new object ( create a new clone of the enemy prefab).
+						Object newObj = Instantiate (EnemyPrefab, transform.position, transform.rotation);
+						if(SingleSpawn){
+							canSpawn = false;
+						}
+						//add the new enemy object to the list.
+						this.lis.Add (newObj);
+					}
+				}
+			}
+			//There are already maximum amounts of spawns.
+			else
+			{
+				//iterate through the list of objects and remove objects that have been destroyed from the list.
+				for(int i = 0; i < this.lis.Count;i++)
+				{
+					if(this.lis[i] == null)
+					{
+						this.lis.Remove(this.lis[i]);
+					}
+				}
+			}
+		}
 	}
 }
